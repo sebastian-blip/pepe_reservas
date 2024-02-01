41f01256d01602fa17471249c43a6a134a3a68a2
Sebastian Betancourth <betancourth.ingenieria@gmail.com>
2024-01-31 23:00:35 -0500

    feat>agregar tabla

--- app/routers/auth/auth.py
+++ app/routers/auth/auth.py
@@ -31,43 +31,29 @@ async def form_login(request: Request, verificado: str = 'si'):
     return respuesta
 
 
+
+@auth_router.get(
+    "/reserva", status_code=200)
+async def form_login(request: Request):
+    """
+
+    """
+
+    respuesta = templates.TemplateResponse('reserva/index.html',
+                                           {'request': request})
+
+    return respuesta
+
+
 @auth_router.post(
     "/login", status_code=200)
 async def login(request: Request):
     """
 
     """
-    good_pass = False
-
-    try:
-        mongo_bd = get_bd()
-        user_collection = mongo_bd['users']
-
-        form_data = await request.form()
-
-        find_user = {'username': form_data.get('username')}
-        user = await user_collection.find_one(find_user)
-        if user:
-            good_pass = verificar_usuario(form_data.get('password'), user['password'])
-    except Exception as e:
-        msg = 'error desconocido'
-        log.error(str(traceback.format_exc()))
-        res = dict(success=False, data=None, msg=msg)
-        response = JSONResponse(content=res, status_code=500)
-    else:
-        if good_pass:
-            token = create_access_token(user['username'])
-            response = RedirectResponse("www.google.com", status_code=302)
-            response.set_cookie(
-                key=Sesion.cookie_name,
-                value=token,
-                expires=Sesion.expiracion_cookie,
-            )
-
-        else:
-            response = RedirectResponse(
-                "/auth/form-login" + f"?verificado=no",
-                status_code=302)
+    response = RedirectResponse(
+        "/auth/reserva",
+        status_code=302)
 
     return response
 
